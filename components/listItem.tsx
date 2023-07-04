e29e91c777a6f74d1d19d7238d8d0466ec0e6f52
shyamtiwari02062001 <shyam02062001@gmail.com>
2023-07-04 07:16:20 +0530

    ListItem is ready

---
+++ components/listItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+const ListItem = () => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.title}>title</Text>
+      <View style={styles.divider} />
+      <Text style={styles.subtitle}>subtitle</Text>
+    </View>
+  );
+};
+export default ListItem;
+const styles = StyleSheet.create({
+  card: {
+    width: '90%',
+    display: 'flex',
+    alignSelf: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    margin: 20,
+    backgroundColor: '#fff',
+    height: 100,
+    borderRadius: 20,
+    elevation: 20,
+    shadowColor: '#000',
+  },
+  title: {
+    color: '#000',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  divider: {
+    width: '100%',
+    height: 1,
+    backgroundColor: 'red',
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: '#000',
+    fontSize: 20,
+  },
+});
